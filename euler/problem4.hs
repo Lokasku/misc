3b840046041c75dc314b3e9b0717e0ae9f730311
Lukhell <lukhell@proton.me>
2023-03-29 16:59:28 +0200

    Problem 4.

--- euler/problem4.hs
+++ euler/problem4.hs
@@ -1,2 +1,6 @@
 -- https://projecteuler.net/problem=4
 
+palindromeProduct :: Int -> [Int]
+palindromeProduct limit = foldr (\x r -> (foldr (\y r2 -> if (read $ reverse $ show (x*y)) == x*y then (x*y) : r2 else r2) [] [100..limit]) ++ r) [] [100..limit]
+
+higherPalindrome = maximum $ palindromeProduct 1000
